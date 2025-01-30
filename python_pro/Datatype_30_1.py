5bdb3dadb8347854bb65e16ca5aaa3f73c7620e8
Japan Kansara <japan.kansara@aliansoftware.com>
2025-01-30 12:29:58 +0530

    Up to Operator

---
+++ python_pro/Datatype_30_1.py
@@ -0,0 +1,24 @@
+#Sequence
+
+#List
+a = [1, 2, "one"]
+print(a)
+
+#Tuple
+a = (1, 2 , "one")
+print(a)
+
+
+#Set
+a = {1, 2, "one"}
+print(a)
+
+#String
+a = "one"
+print(a)
+
+#map
+a = {'1':'japan', '2':'abc'}  #1 is key , abc is value
+print(a)
+print(a.values())
+print(a.get('1')) #get data from key
---
+++ python_pro/Variable_30_1.py
@@ -0,0 +1,28 @@
+#Variable
+
+x = 10
+x = "Hello Variable"
+print(x)
+
+a, b, c = 1, 2, 3
+print(a, b, c)
+
+a, b, c = 1, 1.01, "Hello"
+print(a, b, c)
+
+#Typecasting
+
+a,b,c = 10, 20 ,30
+d = str(a)
+e = float(b)
+print(d,e)
+
+#swapping value
+a, b = 5, 10
+a, b = b, a
+print(a, b)
+
+#length of string
+a = "Python"
+length = len(a)
+print("Length of the a:", length)
